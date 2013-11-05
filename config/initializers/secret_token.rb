81fc6618f3187d41ec0a730b88aca0d904abcbe1
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-11-05 16:12:46 -0300

    [initial commit]
      - Create application with rails 4.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Torca::Application.config.secret_key_base = 'a81eb6d0decc47b04f02ec4e96aa07a9178f0d4497283056a51d51203e44d4d41178bf9f8100746dd4b91c6d9fbeb5606b2f3e6281a5980a4f2041ae739a26de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Torca::Application.config.session_store :cookie_store, key: '_torca_session'
