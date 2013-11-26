9a433b7892bb9ea1f036545361425e6eb813ee5e
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-11-26 18:14:23 -0300

    [Fix place selector in delivery step]
      - Add condition for validate that stock item (store) verify that count_on_hand to be >= maximum quantity of stock_location.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     Spree::StockItem.find(
       :all,
       :select => "*, count(stock_location_id) as num",
-      :conditions => ["variant_id IN(?) and count_on_hand > 0",variant_ids(ship_form)],
+      :conditions => ["variant_id IN(?) and count_on_hand > ?",
+        variant_ids(ship_form), max_quantity(ship_form) - 1],
       :group => 'stock_location_id',
       :having => "num > #{ship_form.object.manifest.count - 1}"
       ).collect { |m| [m.stock_location.name, m.stock_location.id] }
@@ -11,6 +12,10 @@ module ApplicationHelper
 
   def variant_ids(ship_form)
     ship_form.object.manifest.collect {|m| m.variant.id }
+  end
+
+  def max_quantity(ship_form)
+    ship_form.object.manifest.max_by(&:quantity).quantity
   end
 
 end
